8ca824adf847a3aaf99c9c7ca1ed5490446c890b
hemantdidel <hemantdidel2002@gmail.com>
2026-06-23 11:27:14 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,34 +1,42 @@
+// Firebase App
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+// Firestore
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  serverTimestamp,
-  query,
-  orderBy,
-  limit,
-  getDocs
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    query,
+    orderBy,
+    limit,
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
+// Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCND6qyK5D4k_99fKUdta1pm-JxC5GF4Wo",
-  authDomain: "pyq-quiz-master.firebaseapp.com",
-  projectId: "pyq-quiz-master",
-  storageBucket: "pyq-quiz-master.firebasestorage.app",
-  messagingSenderId: "382920239080",
-  appId: "1:382920239080:web:1d739b45c3d5e011d0e65d"
+    apiKey: "AIzaSyCND6qyK5D4k_99fKUdta1pm-JxC5GF4Wo",
+    authDomain: "pyq-quiz-master.firebaseapp.com",
+    projectId: "pyq-quiz-master",
+    storageBucket: "pyq-quiz-master.firebasestorage.app",
+    messagingSenderId: "382920239080",
+    appId: "1:382920239080:web:1d739b45c3d5e011d0e65d"
 };
 
+// Initialize
 const app = initializeApp(firebaseConfig);
+
+// Firestore
 const db = getFirestore(app);
 
-window.db = db;
-window.fb = {
-  collection,
-  addDoc,
-  serverTimestamp,
-  query,
-  orderBy,
-  limit,
-  getDocs
+// Export
+export {
+    db,
+    collection,
+    addDoc,
+    getDocs,
+    query,
+    orderBy,
+    limit,
+    serverTimestamp
 };
