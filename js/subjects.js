6ddc95fe7a005dc9851ed651d0ef0225353de7c8
hemantdidel <hemantdidel2002@gmail.com>
2026-06-27 13:16:37 +0530

    Update subjects.js

--- js/subjects.js
+++ js/subjects.js
@@ -1,4 +1,33 @@
 const container = document.getElementById("subjectContainer");
+// ===========================
+// SEO
+// ===========================
+
+document.title =
+"Subject Wise Practice | PYQ Quiz Master";
+
+document.querySelector('meta[name="description"]')
+.setAttribute(
+"content",
+"Practice Rajasthan GK, India GK, Math, Science, Computer and Reasoning topic-wise with free online mock tests."
+);
+
+document.querySelector('meta[name="keywords"]')
+.setAttribute(
+"content",
+"Rajasthan GK, India GK, Math, Reasoning, Science, Computer, Subject Wise MCQ"
+);
+
+const canonical =
+document.querySelector('link[rel="canonical"]');
+
+if(canonical){
+
+canonical.href =
+window.location.origin +
+window.location.pathname;
+
+}
 
 fetch("data/subjects.json")
 .then(res => res.json())
